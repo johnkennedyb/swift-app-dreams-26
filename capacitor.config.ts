4b169cce0b1de3b5125c00badecd5c0a3535f842
Johnkennedy <deepcodesystems@gmail.com>
2025-06-28 11:41:41 +0100

    push

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.appacus',
   appName: 'Appacus',
   webDir: 'dist',
-  server: {
-    url: 'https://7d0efb0f-c56f-490e-9127-da573c76b6a4.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // server: {
+  //   url: 'https://7d0efb0f-c56f-490e-9127-da573c76b6a4.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
